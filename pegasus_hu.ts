f295bb304d05f6144a3446e3914c5707ead83e2f
Mátyás Mustoha <matyas.mustoha@gmail.com>
2017-06-18 14:57:45 +0200

    Added Hungarian translation

---
+++ pegasus_hu.ts
@@ -0,0 +1,299 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="hu_HU">
+<context>
+    <name>ApiObject</name>
+    <message>
+        <location filename="../src/backend/Api.cpp" line="98"/>
+        <source>Invalid platform index #%1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/backend/Api.cpp" line="121"/>
+        <source>Could not set game index, the current platform is undefined!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/backend/Api.cpp" line="127"/>
+        <source>Invalid game index #%1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/backend/Api.cpp" line="144"/>
+        <source>The current platform is undefined, you can&apos;t launch any games!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/backend/Api.cpp" line="148"/>
+        <source>The current game is undefined, you can&apos;t launch it!</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ApiParts::Meta</name>
+    <message>
+        <location filename="../src/backend/api_parts/ApiMeta.cpp" line="36"/>
+        <source>Data files loaded in %1ms</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ApiParts::System</name>
+    <message>
+        <location filename="../src/backend/api_parts/ApiSystem.cpp" line="39"/>
+        <source>Rebooting...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/backend/api_parts/ApiSystem.cpp" line="52"/>
+        <source>Shutting down...</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ConfigScreen</name>
+    <message>
+        <location filename="../src/frontend/gamepad/ConfigScreen.qml" line="108"/>
+        <source>No gamepads connected</source>
+        <translation>Nincs gamepad csatlakoztatva</translation>
+    </message>
+    <message>
+        <location filename="../src/frontend/gamepad/ConfigScreen.qml" line="166"/>
+        <source>left back</source>
+        <translation>bal hátsó</translation>
+    </message>
+    <message>
+        <location filename="../src/frontend/gamepad/ConfigScreen.qml" line="177"/>
+        <location filename="../src/frontend/gamepad/ConfigScreen.qml" line="344"/>
+        <source>shoulder</source>
+        <translation>vállgomb</translation>
+    </message>
+    <message>
+        <location filename="../src/frontend/gamepad/ConfigScreen.qml" line="192"/>
+        <location filename="../src/frontend/gamepad/ConfigScreen.qml" line="358"/>
+        <source>trigger</source>
+        <translation>ravasz</translation>
+    </message>
+    <message>
+        <location filename="../src/frontend/gamepad/ConfigScreen.qml" line="208"/>
+        <source>dpad</source>
+        <translation>iránygombok</translation>
+    </message>
+    <message>
+        <location filename="../src/frontend/gamepad/ConfigScreen.qml" line="217"/>
+        <source>up</source>
+        <translation>fel</translation>
+    </message>
+    <message>
+        <location filename="../src/frontend/gamepad/ConfigScreen.qml" line="232"/>
+        <source>down</source>
+        <translation>le</translation>
+    </message>
+    <message>
+        <location filename="../src/frontend/gamepad/ConfigScreen.qml" line="247"/>
+        <source>left</source>
+        <translation>balra</translation>
+    </message>
+    <message>
+        <location filename="../src/frontend/gamepad/ConfigScreen.qml" line="262"/>
+        <source>right</source>
+        <translation>jobbra</translation>
+    </message>
+    <message>
+        <location filename="../src/frontend/gamepad/ConfigScreen.qml" line="278"/>
+        <source>left stick</source>
+        <translation>bal kar</translation>
+    </message>
+    <message>
+        <location filename="../src/frontend/gamepad/ConfigScreen.qml" line="288"/>
+        <location filename="../src/frontend/gamepad/ConfigScreen.qml" line="451"/>
+        <source>x axis</source>
+        <translation>x tengely</translation>
+    </message>
+    <message>
+        <location filename="../src/frontend/gamepad/ConfigScreen.qml" line="303"/>
+        <location filename="../src/frontend/gamepad/ConfigScreen.qml" line="465"/>
+        <source>y axis</source>
+        <translation>y tengely</translation>
+    </message>
+    <message>
+        <location filename="../src/frontend/gamepad/ConfigScreen.qml" line="318"/>
+        <location filename="../src/frontend/gamepad/ConfigScreen.qml" line="479"/>
+        <source>press</source>
+        <translation>lenyomás</translation>
+    </message>
+    <message>
+        <location filename="../src/frontend/gamepad/ConfigScreen.qml" line="333"/>
+        <source>right back</source>
+        <translation>jobb hátsó</translation>
+    </message>
+    <message>
+        <location filename="../src/frontend/gamepad/ConfigScreen.qml" line="373"/>
+        <source>abxy</source>
+        <translation>abxy</translation>
+    </message>
+    <message>
+        <location filename="../src/frontend/gamepad/ConfigScreen.qml" line="440"/>
+        <source>right stick</source>
+        <translation>jobb kar</translation>
+    </message>
+    <message>
+        <location filename="../src/frontend/gamepad/ConfigScreen.qml" line="500"/>
+        <source>center</source>
+        <translation>közép</translation>
+    </message>
+    <message>
+        <location filename="../src/frontend/gamepad/ConfigScreen.qml" line="509"/>
+        <source>select</source>
+        <translation>select</translation>
+    </message>
+    <message>
+        <location filename="../src/frontend/gamepad/ConfigScreen.qml" line="525"/>
+        <source>guide</source>
+        <translation>guide</translation>
+    </message>
+    <message>
+        <location filename="../src/frontend/gamepad/ConfigScreen.qml" line="540"/>
+        <source>start</source>
+        <translation>start</translation>
+    </message>
+    <message>
+        <location filename="../src/frontend/gamepad/ConfigScreen.qml" line="632"/>
+        <source>hold down to quit</source>
+        <translation>kilépéshez tartst lenyomva</translation>
+    </message>
+</context>
+<context>
+    <name>MainMenuPanel</name>
+    <message>
+        <location filename="../src/frontend/MainMenuPanel.qml" line="43"/>
+        <source>Settings</source>
+        <translation>Beállítások</translation>
+    </message>
+    <message>
+        <location filename="../src/frontend/MainMenuPanel.qml" line="52"/>
+        <source>Controls</source>
+        <translation>Irányítás</translation>
+    </message>
+    <message>
+        <location filename="../src/frontend/MainMenuPanel.qml" line="62"/>
+        <source>Quit</source>
+        <translation>Kilépés</translation>
+    </message>
+    <message>
+        <location filename="../src/frontend/MainMenuPanel.qml" line="85"/>
+        <source>Shutdown</source>
+        <translation>Leállítás</translation>
+    </message>
+    <message>
+        <location filename="../src/frontend/MainMenuPanel.qml" line="94"/>
+        <source>Reboot</source>
+        <translation>Újraindítás</translation>
+    </message>
+    <message>
+        <location filename="../src/frontend/MainMenuPanel.qml" line="103"/>
+        <source>Exit Pegasus</source>
+        <translation>Kilépés</translation>
+    </message>
+</context>
+<context>
+    <name>ProcessLauncher</name>
+    <message>
+        <location filename="../src/backend/ProcessLauncher.cpp" line="36"/>
+        <source>Executing command: `%1`</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/backend/ProcessLauncher.cpp" line="105"/>
+        <source>Process %1 started</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/backend/ProcessLauncher.cpp" line="114"/>
+        <source>Could not run the command `%1`; either the invoked program is missing, or you don&apos;t have the permission to run it.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/backend/ProcessLauncher.cpp" line="120"/>
+        <source>The external program `%1` has crashed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/backend/ProcessLauncher.cpp" line="124"/>
+        <source>The command `%1` has not started in a reasonable amount of time</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/backend/ProcessLauncher.cpp" line="134"/>
+        <source>Running the command `%1` failed due to an unknown error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/backend/ProcessLauncher.cpp" line="147"/>
+        <source>The external program has finished cleanly, with exit code %2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/backend/ProcessLauncher.cpp" line="151"/>
+        <source>The external program has crashed on exit, with exit code %2</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../src/app/main.cpp" line="38"/>
+        <source>A cross platform, customizable graphical frontend for launching emulators
+and managing your game collection.</source>
+        <translation>Többféle rendszerre elérhető, személyre szabható grafikus felület
+játékgyűjteményed kezeléséhez és emulátorok használatához.</translation>
+    </message>
+    <message>
+        <location filename="../src/app/main.cpp" line="47"/>
+        <source>Sorry, you cannot create this type in QML.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/backend/es2/Es2Gamelist.cpp" line="38"/>
+        <source>ES2 gamelist for platform `%1` not found</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/backend/es2/Es2Gamelist.cpp" line="46"/>
+        <location filename="../src/backend/es2/Es2Systems.cpp" line="44"/>
+        <source>Could not open `%1`</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/backend/es2/Es2Gamelist.cpp" line="62"/>
+        <source>Found `%1`</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/backend/es2/Es2Gamelist.cpp" line="111"/>
+        <location filename="../src/backend/es2/Es2Systems.cpp" line="82"/>
+        <source>Could not parse `%1`</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/backend/es2/Es2Gamelist.cpp" line="116"/>
+        <source>`%1` does not have a `&lt;gameList&gt;` root node!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/backend/es2/Es2Systems.cpp" line="37"/>
+        <source>ES2 system config not found</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/backend/es2/Es2Systems.cpp" line="87"/>
+        <source>`%1` does not have a `&lt;systemList&gt;` root node!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/backend/es2/Es2Systems.cpp" line="123"/>
+        <source>Required parameter &lt;%1&gt; is missing or empty in a &lt;system&gt; node</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
