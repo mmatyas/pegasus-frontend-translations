0e70ac4f6dc8d7ebb2ad3c3009b8907ebd3f3ed5
Mátyás Mustoha <matyas.mustoha@gmail.com>
2018-04-10 20:42:07 +0200

    Updated Hungarian translation

--- pegasus_hu.ts
+++ pegasus_hu.ts
@@ -29,112 +29,112 @@
     <message>
         <location filename="../src/frontend/menu/GamepadScreen.qml" line="110"/>
         <source>No gamepads connected</source>
-        <translation type="unfinished">Nincs gamepad csatlakoztatva</translation>
+        <translation>Nincs gamepad csatlakoztatva</translation>
     </message>
     <message>
         <location filename="../src/frontend/menu/GamepadScreen.qml" line="168"/>
         <source>left back</source>
-        <translation type="unfinished">bal hátsó</translation>
+        <translation>bal hátsó</translation>
     </message>
     <message>
         <location filename="../src/frontend/menu/GamepadScreen.qml" line="179"/>
         <location filename="../src/frontend/menu/GamepadScreen.qml" line="346"/>
         <source>shoulder</source>
-        <translation type="unfinished">vállgomb</translation>
+        <translation>vállgomb</translation>
     </message>
     <message>
         <location filename="../src/frontend/menu/GamepadScreen.qml" line="194"/>
         <location filename="../src/frontend/menu/GamepadScreen.qml" line="360"/>
         <source>trigger</source>
-        <translation type="unfinished">ravasz</translation>
+        <translation>ravasz</translation>
     </message>
     <message>
         <location filename="../src/frontend/menu/GamepadScreen.qml" line="210"/>
         <source>dpad</source>
-        <translation type="unfinished">iránygombok</translation>
+        <translation>iránygombok</translation>
     </message>
     <message>
         <location filename="../src/frontend/menu/GamepadScreen.qml" line="219"/>
         <source>up</source>
-        <translation type="unfinished">fel</translation>
+        <translation>fel</translation>
     </message>
     <message>
         <location filename="../src/frontend/menu/GamepadScreen.qml" line="234"/>
         <source>down</source>
-        <translation type="unfinished">le</translation>
+        <translation>le</translation>
     </message>
     <message>
         <location filename="../src/frontend/menu/GamepadScreen.qml" line="249"/>
         <source>left</source>
-        <translation type="unfinished">balra</translation>
+        <translation>balra</translation>
     </message>
     <message>
         <location filename="../src/frontend/menu/GamepadScreen.qml" line="264"/>
         <source>right</source>
-        <translation type="unfinished">jobbra</translation>
+        <translation>jobbra</translation>
     </message>
     <message>
         <location filename="../src/frontend/menu/GamepadScreen.qml" line="280"/>
         <source>left stick</source>
-        <translation type="unfinished">bal kar</translation>
+        <translation>bal kar</translation>
     </message>
     <message>
         <location filename="../src/frontend/menu/GamepadScreen.qml" line="290"/>
         <location filename="../src/frontend/menu/GamepadScreen.qml" line="453"/>
         <source>x axis</source>
-        <translation type="unfinished">x tengely</translation>
+        <translation>x tengely</translation>
     </message>
     <message>
         <location filename="../src/frontend/menu/GamepadScreen.qml" line="305"/>
         <location filename="../src/frontend/menu/GamepadScreen.qml" line="467"/>
         <source>y axis</source>
-        <translation type="unfinished">y tengely</translation>
+        <translation>y tengely</translation>
     </message>
     <message>
         <location filename="../src/frontend/menu/GamepadScreen.qml" line="320"/>
         <location filename="../src/frontend/menu/GamepadScreen.qml" line="481"/>
         <source>press</source>
-        <translation type="unfinished">lenyomás</translation>
+        <translation>lenyomás</translation>
     </message>
     <message>
         <location filename="../src/frontend/menu/GamepadScreen.qml" line="335"/>
         <source>right back</source>
-        <translation type="unfinished">jobb hátsó</translation>
+        <translation>jobb hátsó</translation>
     </message>
     <message>
         <location filename="../src/frontend/menu/GamepadScreen.qml" line="375"/>
         <source>abxy</source>
-        <translation type="unfinished">abxy</translation>
+        <translation>abxy</translation>
     </message>
     <message>
         <location filename="../src/frontend/menu/GamepadScreen.qml" line="442"/>
         <source>right stick</source>
-        <translation type="unfinished">jobb kar</translation>
+        <translation>jobb kar</translation>
     </message>
     <message>
         <location filename="../src/frontend/menu/GamepadScreen.qml" line="502"/>
         <source>center</source>
-        <translation type="unfinished">közép</translation>
+        <translation>közép</translation>
     </message>
     <message>
         <location filename="../src/frontend/menu/GamepadScreen.qml" line="511"/>
         <source>select</source>
-        <translation type="unfinished">select</translation>
+        <translation>select</translation>
     </message>
     <message>
         <location filename="../src/frontend/menu/GamepadScreen.qml" line="527"/>
         <source>guide</source>
-        <translation type="unfinished">guide</translation>
+        <translation>guide</translation>
     </message>
     <message>
         <location filename="../src/frontend/menu/GamepadScreen.qml" line="542"/>
         <source>start</source>
-        <translation type="unfinished">start</translation>
+        <translation>start</translation>
     </message>
     <message>
         <location filename="../src/frontend/menu/GamepadScreen.qml" line="634"/>
         <source>hold down to quit</source>
-        <translation type="unfinished">kilépéshez tartst lenyomva</translation>
+        <translation>kilépéshez tartst lenyomva</translation>
     </message>
 </context>
 <context>
@@ -142,12 +142,12 @@
     <message>
         <location filename="../src/frontend/dialogs/GenericOkCancelDialog.qml" line="164"/>
         <source>Ok</source>
-        <translation type="unfinished">Rendben</translation>
+        <translation>Rendben</translation>
     </message>
     <message>
         <location filename="../src/frontend/dialogs/GenericOkCancelDialog.qml" line="196"/>
         <source>Cancel</source>
-        <translation type="unfinished">Mégse</translation>
+        <translation>Mégse</translation>
     </message>
 </context>
 <context>
@@ -188,22 +188,22 @@
     <message>
         <location filename="../src/frontend/messages/NoGamesError.qml" line="42"/>
         <source>No games found :(</source>
-        <translation type="unfinished">Nem találtam játékot :(</translation>
+        <translation>Nem találtam játékot :(</translation>
     </message>
     <message>
         <location filename="../src/frontend/messages/NoGamesError.qml" line="57"/>
         <source>Pegasus couldn&apos;t find any games on your device. If you have not set up Pegasus yet, you can find the documentation here: &lt;i&gt;%1&lt;/i&gt;.&lt;br&gt;If you still see this message, make sure your config files are readable, exist in one of the expected locations and are in the expected format.</source>
-        <translation type="unfinished"></translation>
+        <translation>A program nem talált egyetlen játékot sem a rendszeren. Ha még nem végezted el a szükséges beállításokat, ezen az oldalon találsz segítséget: &lt;i&gt;%1&lt;/i&gt;.&lt;br&gt;Ha még ezután is megjelenik ez az üzenet, ellenőrizd hogy a konfigurációs fájljaid olvashatóak, illetve a megfelelő helyen és formátumban vannak-e.</translation>
     </message>
     <message>
         <location filename="../src/frontend/messages/NoGamesError.qml" line="75"/>
         <source>Please see the log file for more details.</source>
-        <translation type="unfinished">További információkért lásd az alábbi naplófájlt.</translation>
+        <translation>További információkért lásd az alábbi naplófájlt.</translation>
     </message>
     <message>
         <location filename="../src/frontend/messages/NoGamesError.qml" line="87"/>
         <source>You can find it here:&lt;pre&gt;%1&lt;/pre&gt;</source>
-        <translation type="unfinished">Itt találod:&lt;pre&gt;%1&lt;/pre&gt;</translation>
+        <translation>Itt találod:&lt;pre&gt;%1&lt;/pre&gt;</translation>
     </message>
 </context>
 <context>
@@ -211,12 +211,12 @@
     <message>
         <location filename="../src/frontend/dialogs/RebootDialog.qml" line="25"/>
         <source>Reboot</source>
-        <translation type="unfinished">Újraindítás</translation>
+        <translation>Újraindítás</translation>
     </message>
     <message>
         <location filename="../src/frontend/dialogs/RebootDialog.qml" line="26"/>
         <source>The system will reboot. Are you sure?</source>
-        <translation type="unfinished">Biztos újra akarod indítani a rendszert?</translation>
+        <translation>Biztos újra akarod indítani a rendszert?</translation>
     </message>
 </context>
 <context>
@@ -224,27 +224,27 @@
     <message>
         <location filename="../src/frontend/menu/SettingsScreen.qml" line="49"/>
         <source>Settings</source>
-        <translation type="unfinished">Beállítások</translation>
+        <translation>Beállítások</translation>
     </message>
     <message>
         <location filename="../src/frontend/menu/SettingsScreen.qml" line="89"/>
         <source>Language</source>
-        <translation type="unfinished">Nyelv</translation>
+        <translation>Nyelv</translation>
     </message>
     <message>
         <location filename="../src/frontend/menu/SettingsScreen.qml" line="109"/>
         <source>Theme</source>
-        <translation type="unfinished">Téma</translation>
+        <translation>Téma</translation>
     </message>
     <message>
         <location filename="../src/frontend/menu/SettingsScreen.qml" line="129"/>
         <source>Fullscreen mode</source>
-        <translation type="unfinished">Teljes képernyős mód</translation>
+        <translation>Teljes képernyős mód</translation>
     </message>
     <message>
         <location filename="../src/frontend/menu/SettingsScreen.qml" line="130"/>
         <source>On some platforms this setting may have no effect</source>
-        <translation type="unfinished">Bizonyos rendszereken ennek a beállításnak nincs hatása</translation>
+        <translation>Bizonyos rendszereken ennek a beállításnak nincs hatása</translation>
     </message>
 </context>
 <context>
@@ -252,12 +252,12 @@
     <message>
         <location filename="../src/frontend/dialogs/ShutdownDialog.qml" line="25"/>
         <source>Shutdown</source>
-        <translation type="unfinished">Leállítás</translation>
+        <translation>Leállítás</translation>
     </message>
     <message>
         <location filename="../src/frontend/dialogs/ShutdownDialog.qml" line="26"/>
         <source>The system will shut down. Are you sure?</source>
-        <translation type="unfinished">Biztos le akarod állítani a rendszert?</translation>
+        <translation>Biztos le akarod állítani a rendszert?</translation>
     </message>
 </context>
 <context>
@@ -266,17 +266,18 @@
         <location filename="../src/frontend/SplashScreen.qml" line="53"/>
         <source>games
 found</source>
-        <translation type="unfinished"></translation>
+        <translation>játék
+megtalálva</translation>
     </message>
     <message>
         <location filename="../src/frontend/SplashScreen.qml" line="87"/>
         <source>looking for games</source>
-        <translation type="unfinished"></translation>
+        <translation>játékok keresése</translation>
     </message>
     <message>
         <location filename="../src/frontend/SplashScreen.qml" line="113"/>
         <source>looking for metadata</source>
-        <translation type="unfinished"></translation>
+        <translation>metaadatok keresése</translation>
     </message>
 </context>
 <context>
@@ -284,22 +285,22 @@ found</source>
     <message>
         <location filename="../src/frontend/messages/ThemeError.qml" line="42"/>
         <source>Theme loading failed :(</source>
-        <translation type="unfinished">Téma betöltés sikertelen :(</translation>
+        <translation>Téma betöltés sikertelen :(</translation>
     </message>
     <message>
         <location filename="../src/frontend/messages/ThemeError.qml" line="57"/>
         <source>Pegasus tried to load the selected theme (%1), but failed. This may happen when you try to load an outdated theme, or when there&apos;s a bug in its code.</source>
-        <translation type="unfinished">Pegasus megpróbáta betölteni a kiválasztott témát (%1), de nem sikerült. Ez akkor fordul elő, ha egy elavult témát próbálsz megnyitni, vagy amikor a téma forráskódja hibás.</translation>
+        <translation>A program megpróbálta betölteni a kiválasztott témát (%1), de nem sikerült. Ez akkor fordul elő, ha egy elavult témát próbálsz megnyitni, vagy a téma forráskódja hibás.</translation>
     </message>
     <message>
         <location filename="../src/frontend/messages/ThemeError.qml" line="73"/>
         <source>For now, please select a different theme from the main menu.</source>
-        <translation type="unfinished">Addig is, kérlek válassz egy másik témát a főmenüben.</translation>
+        <translation>Addig is, kérlek válassz egy másik témát a főmenüben.</translation>
     </message>
     <message>
         <location filename="../src/frontend/messages/ThemeError.qml" line="85"/>
         <source>You might find more details in the log file:&lt;pre&gt;%1&lt;/pre&gt;</source>
-        <translation type="unfinished">A naplófájlban további részleteket találthatsz:&lt;pre&gt;%1&lt;/pre&gt;</translation>
+        <translation>A naplófájlban lehet, hogy több információt is látsz:&lt;pre&gt;%1&lt;/pre&gt;</translation>
     </message>
 </context>
 </TS>
