20d48baf10041a5b57f2e86c610f427da8dc3d13
Mátyás Mustoha <matyas.mustoha@gmail.com>
2018-04-15 18:42:51 +0200

    Added Spanish translation
    Created by sikboy.

---
+++ pegasus_es.ts
@@ -0,0 +1,306 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="es" sourcelanguage="en">
+<context>
+    <name>FilterPanel</name>
+    <message>
+        <location filename="../src/themes/pegasus-grid/filter_panel/FilterPanel.qml" line="53"/>
+        <source>Filters</source>
+        <translation>Filtros</translation>
+    </message>
+    <message>
+        <location filename="../src/themes/pegasus-grid/filter_panel/FilterPanel.qml" line="66"/>
+        <source>title</source>
+        <translation>título</translation>
+    </message>
+    <message>
+        <location filename="../src/themes/pegasus-grid/filter_panel/FilterPanel.qml" line="81"/>
+        <source>Favorites</source>
+        <translation>Favoritos</translation>
+    </message>
+    <message>
+        <location filename="../src/themes/pegasus-grid/filter_panel/FilterPanel.qml" line="94"/>
+        <source>Multiplayer</source>
+        <translation>Multijugador</translation>
+    </message>
+</context>
+<context>
+    <name>GamepadScreen</name>
+    <message>
+        <location filename="../src/frontend/menu/GamepadScreen.qml" line="110"/>
+        <source>No gamepads connected</source>
+        <translation>No hay mandos conectados</translation>
+    </message>
+    <message>
+        <location filename="../src/frontend/menu/GamepadScreen.qml" line="168"/>
+        <source>left back</source>
+        <translation>traseros izquierda</translation>
+    </message>
+    <message>
+        <location filename="../src/frontend/menu/GamepadScreen.qml" line="179"/>
+        <location filename="../src/frontend/menu/GamepadScreen.qml" line="346"/>
+        <source>shoulder</source>
+        <translation>gatillo delantero</translation>
+    </message>
+    <message>
+        <location filename="../src/frontend/menu/GamepadScreen.qml" line="194"/>
+        <location filename="../src/frontend/menu/GamepadScreen.qml" line="360"/>
+        <source>trigger</source>
+        <translation>gatillo trasero</translation>
+    </message>
+    <message>
+        <location filename="../src/frontend/menu/GamepadScreen.qml" line="210"/>
+        <source>dpad</source>
+        <translation>direccional</translation>
+    </message>
+    <message>
+        <location filename="../src/frontend/menu/GamepadScreen.qml" line="219"/>
+        <source>up</source>
+        <translation>arriba</translation>
+    </message>
+    <message>
+        <location filename="../src/frontend/menu/GamepadScreen.qml" line="234"/>
+        <source>down</source>
+        <translation>abajo</translation>
+    </message>
+    <message>
+        <location filename="../src/frontend/menu/GamepadScreen.qml" line="249"/>
+        <source>left</source>
+        <translation>izquierda</translation>
+    </message>
+    <message>
+        <location filename="../src/frontend/menu/GamepadScreen.qml" line="264"/>
+        <source>right</source>
+        <translation>derecha</translation>
+    </message>
+    <message>
+        <location filename="../src/frontend/menu/GamepadScreen.qml" line="280"/>
+        <source>left stick</source>
+        <translation>palanca izquierda</translation>
+    </message>
+    <message>
+        <location filename="../src/frontend/menu/GamepadScreen.qml" line="290"/>
+        <location filename="../src/frontend/menu/GamepadScreen.qml" line="453"/>
+        <source>x axis</source>
+        <translation>eje x</translation>
+    </message>
+    <message>
+        <location filename="../src/frontend/menu/GamepadScreen.qml" line="305"/>
+        <location filename="../src/frontend/menu/GamepadScreen.qml" line="467"/>
+        <source>y axis</source>
+        <translation>eje y</translation>
+    </message>
+    <message>
+        <location filename="../src/frontend/menu/GamepadScreen.qml" line="320"/>
+        <location filename="../src/frontend/menu/GamepadScreen.qml" line="481"/>
+        <source>press</source>
+        <translation>presiona</translation>
+    </message>
+    <message>
+        <location filename="../src/frontend/menu/GamepadScreen.qml" line="335"/>
+        <source>right back</source>
+        <translation>traseros derecha</translation>
+    </message>
+    <message>
+        <location filename="../src/frontend/menu/GamepadScreen.qml" line="375"/>
+        <source>abxy</source>
+        <translation>abxy</translation>
+    </message>
+    <message>
+        <location filename="../src/frontend/menu/GamepadScreen.qml" line="442"/>
+        <source>right stick</source>
+        <translation>palanca izquierda</translation>
+    </message>
+    <message>
+        <location filename="../src/frontend/menu/GamepadScreen.qml" line="502"/>
+        <source>center</source>
+        <translation>centro</translation>
+    </message>
+    <message>
+        <location filename="../src/frontend/menu/GamepadScreen.qml" line="511"/>
+        <source>select</source>
+        <translation>seleccionar</translation>
+    </message>
+    <message>
+        <location filename="../src/frontend/menu/GamepadScreen.qml" line="527"/>
+        <source>guide</source>
+        <translation>guía</translation>
+    </message>
+    <message>
+        <location filename="../src/frontend/menu/GamepadScreen.qml" line="542"/>
+        <source>start</source>
+        <translation>iniciar</translation>
+    </message>
+    <message>
+        <location filename="../src/frontend/menu/GamepadScreen.qml" line="634"/>
+        <source>hold down to quit</source>
+        <translation>Mantén presionado para cerrar</translation>
+    </message>
+</context>
+<context>
+    <name>GenericOkCancelDialog</name>
+    <message>
+        <location filename="../src/frontend/dialogs/GenericOkCancelDialog.qml" line="164"/>
+        <source>Ok</source>
+        <translation>Aceptar</translation>
+    </message>
+    <message>
+        <location filename="../src/frontend/dialogs/GenericOkCancelDialog.qml" line="196"/>
+        <source>Cancel</source>
+        <translation>Cancelar</translation>
+    </message>
+</context>
+<context>
+    <name>MainMenuPanel</name>
+    <message>
+        <location filename="../src/frontend/menu/MainMenuPanel.qml" line="49"/>
+        <source>Settings</source>
+        <translation>Ajustes</translation>
+    </message>
+    <message>
+        <location filename="../src/frontend/menu/MainMenuPanel.qml" line="60"/>
+        <source>Controls</source>
+        <translation>Controles</translation>
+    </message>
+    <message>
+        <location filename="../src/frontend/menu/MainMenuPanel.qml" line="71"/>
+        <source>Quit</source>
+        <translation>Cerrar</translation>
+    </message>
+    <message>
+        <location filename="../src/frontend/menu/MainMenuPanel.qml" line="77"/>
+        <source>Shutdown</source>
+        <translation>Apagar</translation>
+    </message>
+    <message>
+        <location filename="../src/frontend/menu/MainMenuPanel.qml" line="86"/>
+        <source>Reboot</source>
+        <translation>Reiniciar</translation>
+    </message>
+    <message>
+        <location filename="../src/frontend/menu/MainMenuPanel.qml" line="93"/>
+        <source>Exit Pegasus</source>
+        <translation>Salir de Pegasus</translation>
+    </message>
+</context>
+<context>
+    <name>NoGamesError</name>
+    <message>
+        <location filename="../src/frontend/messages/NoGamesError.qml" line="42"/>
+        <source>No games found :(</source>
+        <translation>No se encontraron juegos:(</translation>
+    </message>
+    <message>
+        <location filename="../src/frontend/messages/NoGamesError.qml" line="57"/>
+        <source>Pegasus couldn&apos;t find any games on your device. If you have not set up Pegasus yet, you can find the documentation here: &lt;i&gt;%1&lt;/i&gt;.&lt;br&gt;If you still see this message, make sure your config files are readable, exist in one of the expected locations and are in the expected format.</source>
+        <translation>Pegasus no encontró juegos en tu sistema. Si aun no haz configurado Pegasus, puedes encontrar la documentacion aquí: &lt;i&gt;%1&lt;/i&gt;.&lt;br&gt;Si aun ves este mensaje, asegurate que tus archivos de configuración sean los correctos, esten en la ubicación correcta y el formato correcto.</translation>
+    </message>
+    <message>
+        <location filename="../src/frontend/messages/NoGamesError.qml" line="75"/>
+        <source>Please see the log file for more details.</source>
+        <translation>Por favor ve el archivo log para mas detalles.</translation>
+    </message>
+    <message>
+        <location filename="../src/frontend/messages/NoGamesError.qml" line="87"/>
+        <source>You can find it here:&lt;pre&gt;%1&lt;/pre&gt;</source>
+        <translation>Lo puedes encontrar aquí:&lt;pre&gt;%1&lt;/pre&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>RebootDialog</name>
+    <message>
+        <location filename="../src/frontend/dialogs/RebootDialog.qml" line="25"/>
+        <source>Reboot</source>
+        <translation>Reiniciar</translation>
+    </message>
+    <message>
+        <location filename="../src/frontend/dialogs/RebootDialog.qml" line="26"/>
+        <source>The system will reboot. Are you sure?</source>
+        <translation>El sistema se reiniciará. ¿Estas seguro?</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsScreen</name>
+    <message>
+        <location filename="../src/frontend/menu/SettingsScreen.qml" line="49"/>
+        <source>Settings</source>
+        <translation>Ajustes</translation>
+    </message>
+    <message>
+        <location filename="../src/frontend/menu/SettingsScreen.qml" line="89"/>
+        <source>Language</source>
+        <translation>Lenguaje</translation>
+    </message>
+    <message>
+        <location filename="../src/frontend/menu/SettingsScreen.qml" line="109"/>
+        <source>Theme</source>
+        <translation>Tema</translation>
+    </message>
+    <message>
+        <location filename="../src/frontend/menu/SettingsScreen.qml" line="129"/>
+        <source>Fullscreen mode</source>
+        <translation>Pantalla completa</translation>
+    </message>
+    <message>
+        <location filename="../src/frontend/menu/SettingsScreen.qml" line="130"/>
+        <source>On some platforms this setting may have no effect</source>
+        <translation>En algunas plataformas este ajuste puede que no surta efecto</translation>
+    </message>
+</context>
+<context>
+    <name>ShutdownDialog</name>
+    <message>
+        <location filename="../src/frontend/dialogs/ShutdownDialog.qml" line="25"/>
+        <source>Shutdown</source>
+        <translation>Apagar</translation>
+    </message>
+    <message>
+        <location filename="../src/frontend/dialogs/ShutdownDialog.qml" line="26"/>
+        <source>The system will shut down. Are you sure?</source>
+        <translation>El sistema se apagará. ¿Estas seguro?</translation>
+    </message>
+</context>
+<context>
+    <name>SplashScreen</name>
+    <message>
+        <location filename="../src/frontend/SplashScreen.qml" line="53"/>
+        <source>games
+found</source>
+        <translation>juegos
+encontrados</translation>
+    </message>
+    <message>
+        <location filename="../src/frontend/SplashScreen.qml" line="87"/>
+        <source>looking for games</source>
+        <translation>buscando juegos</translation>
+    </message>
+    <message>
+        <location filename="../src/frontend/SplashScreen.qml" line="113"/>
+        <source>looking for metadata</source>
+        <translation>buscando metadatos</translation>
+    </message>
+</context>
+<context>
+    <name>ThemeError</name>
+    <message>
+        <location filename="../src/frontend/messages/ThemeError.qml" line="42"/>
+        <source>Theme loading failed :(</source>
+        <translation>Carga del tema falló :(</translation>
+    </message>
+    <message>
+        <location filename="../src/frontend/messages/ThemeError.qml" line="57"/>
+        <source>Pegasus tried to load the selected theme (%1), but failed. This may happen when you try to load an outdated theme, or when there&apos;s a bug in its code.</source>
+        <translation>Pegasus trató de cargar el tema seleccionado (%1), sin éxito. Esto puede ocurrir cuando el tema es obsoleto o cuando hay un error en el código en el tema.</translation>
+    </message>
+    <message>
+        <location filename="../src/frontend/messages/ThemeError.qml" line="73"/>
+        <source>For now, please select a different theme from the main menu.</source>
+        <translation>Por ahora, por favor selecciona otro tema desde el menú principal.</translation>
+    </message>
+    <message>
+        <location filename="../src/frontend/messages/ThemeError.qml" line="85"/>
+        <source>You might find more details in the log file:&lt;pre&gt;%1&lt;/pre&gt;</source>
+        <translation>Puedes encontrar más detalles en el archivo log: &lt;pre&gt;%1&lt;/pre&gt;</translation>
+    </message>
+</context>
+</TS>
