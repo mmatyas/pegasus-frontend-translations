932cbf5dd92a668c2db9ba2c9338a6ccfc1d89a7
SecularSteve <33793273+SecularSteve@users.noreply.github.com>
2020-12-09 11:52:31 +0100

    Fixed language code
    And renamed file

--- pegasus_en-GB.ts
+++ pegasus_en-GB.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="en_UK">
+<TS version="2.1" language="en_GB">
 <context>
     <name>FilePicker</name>
     <message>
