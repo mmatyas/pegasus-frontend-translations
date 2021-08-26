702ec06d450c789088243204094130f9bb77aa28
Mátyás Mustoha <matyas.mustoha@gmail.com>
2021-08-26 19:13:49 +0200

    Updated the Russian translation
    By mrqside

--- pegasus_ru.ts
+++ pegasus_ru.ts
@@ -7,20 +7,31 @@
         <location filename="../src/frontend/menu/settings/gamedireditor/FilePicker.qml" line="79"/>
         <source>Select a collection file below (metadata.pegasus.txt or metadata.txt).
 The directory of the file will then be added to the list.</source>
-        <translation type="unfinished"></translation>
+        <translation>Выберите файл коллекции ниже (metadata.pegasus.txt или metadata.txt).
+Папка с файлом, затем, будет добавлена к списку.</translation>
     </message>
 </context>
 <context>
     <name>FilterPanel</name>
     <message>
-        <location filename="../src/themes/pegasus-theme-grid/layer_filter/FilterPanel.qml" line="56"/>
+        <location filename="../src/themes/pegasus-theme-grid/layer_filter/FilterPanel.qml" line="52"/>
         <source>Filters</source>
         <translation>фильтры</translation>
     </message>
     <message>
-        <location filename="../src/themes/pegasus-theme-grid/layer_filter/FilterPanel.qml" line="74"/>
+        <location filename="../src/themes/pegasus-theme-grid/layer_filter/FilterPanel.qml" line="63"/>
         <source>title</source>
         <translation>название</translation>
+    </message>
+    <message>
+        <location filename="../src/themes/pegasus-theme-grid/layer_filter/FilterPanel.qml" line="78"/>
+        <source>Favorite</source>
+        <translation>Избранные</translation>
+    </message>
+    <message>
+        <location filename="../src/themes/pegasus-theme-grid/layer_filter/FilterPanel.qml" line="87"/>
+        <source>Multiplayer</source>
+        <translation>Многопользовательские</translation>
     </message>
 </context>
 <context>
@@ -29,7 +40,8 @@ The directory of the file will then be added to the list.</source>
         <location filename="../src/frontend/menu/settings/GameDirEditor.qml" line="158"/>
         <source>Pegasus will look for collection files (metadata.pegasus.txt or metadata.txt)
 in the following directories:</source>
-        <translation type="unfinished"></translation>
+        <translation>Pegasus будет искать файлы коллекий (metadata.pegasus.txt или metadata.txt)
+в данных папках:</translation>
     </message>
     <message>
         <location filename="../src/frontend/menu/settings/GameDirEditor.qml" line="238"/>
@@ -39,7 +51,7 @@ in the following directories:</source>
     <message>
         <location filename="../src/frontend/menu/settings/GameDirEditor.qml" line="249"/>
         <source>Remove selected</source>
-        <translation>Удалить выбранное</translation>
+        <translation>Удалить выбранные</translation>
     </message>
 </context>
 <context>
@@ -47,29 +59,29 @@ in the following directories:</source>
     <message>
         <location filename="../src/frontend/menu/settings/GamepadEditor.qml" line="114"/>
         <source>No gamepads connected</source>
-        <translation>Нет подключенных геймпадов</translation>
+        <translation>Нет подключённых геймпадов</translation>
     </message>
     <message>
         <location filename="../src/frontend/menu/settings/GamepadEditor.qml" line="164"/>
         <source>left back</source>
-        <translation>Сзади слева</translation>
+        <translation>СЗАДИ СЛЕВА</translation>
     </message>
     <message>
         <location filename="../src/frontend/menu/settings/GamepadEditor.qml" line="175"/>
         <location filename="../src/frontend/menu/settings/GamepadEditor.qml" line="378"/>
         <source>shoulder</source>
-        <translation>плечное</translation>
+        <translation>бампер</translation>
     </message>
     <message>
         <location filename="../src/frontend/menu/settings/GamepadEditor.qml" line="194"/>
         <location filename="../src/frontend/menu/settings/GamepadEditor.qml" line="396"/>
         <source>trigger</source>
-        <translation>Курок</translation>
+        <translation>курок</translation>
     </message>
     <message>
         <location filename="../src/frontend/menu/settings/GamepadEditor.qml" line="214"/>
         <source>dpad</source>
-        <translation>D-Pad</translation>
+        <translation>КРЕСТОВИНА</translation>
     </message>
     <message>
         <location filename="../src/frontend/menu/settings/GamepadEditor.qml" line="223"/>
@@ -94,45 +106,45 @@ in the following directories:</source>
     <message>
         <location filename="../src/frontend/menu/settings/GamepadEditor.qml" line="300"/>
         <source>left stick</source>
-        <translation>левый джойстик</translation>
+        <translation>ЛЕВЫЙ СТИК</translation>
     </message>
     <message>
         <location filename="../src/frontend/menu/settings/GamepadEditor.qml" line="310"/>
         <location filename="../src/frontend/menu/settings/GamepadEditor.qml" line="509"/>
         <source>x axis</source>
-        <translation>Ось-X</translation>
+        <translation>ось X</translation>
     </message>
     <message>
         <location filename="../src/frontend/menu/settings/GamepadEditor.qml" line="329"/>
         <location filename="../src/frontend/menu/settings/GamepadEditor.qml" line="527"/>
         <source>y axis</source>
-        <translation>Ось-Y</translation>
+        <translation>ось Y</translation>
     </message>
     <message>
         <location filename="../src/frontend/menu/settings/GamepadEditor.qml" line="348"/>
         <location filename="../src/frontend/menu/settings/GamepadEditor.qml" line="545"/>
         <source>press</source>
-        <translation>нажмите</translation>
+        <translation>нажатие</translation>
     </message>
     <message>
         <location filename="../src/frontend/menu/settings/GamepadEditor.qml" line="367"/>
         <source>right back</source>
-        <translation>Задний правый</translation>
+        <translation>СЗАДИ СПРАВА</translation>
     </message>
     <message>
         <location filename="../src/frontend/menu/settings/GamepadEditor.qml" line="415"/>
         <source>abxy</source>
-        <translation>ABXY</translation>
+        <translation>КНОПКИ ДЕЙСТВИЯ</translation>
     </message>
     <message>
         <location filename="../src/frontend/menu/settings/GamepadEditor.qml" line="498"/>
         <source>right stick</source>
-        <translation>правый джойстик</translation>
+        <translation>ПРАВЫЙ СТИК</translation>
     </message>
     <message>
         <location filename="../src/frontend/menu/settings/GamepadEditor.qml" line="570"/>
         <source>center</source>
-        <translation>центрировать</translation>
+        <translation>ЦЕНТР</translation>
     </message>
     <message>
         <location filename="../src/frontend/menu/settings/GamepadEditor.qml" line="579"/>
@@ -142,7 +154,7 @@ in the following directories:</source>
     <message>
         <location filename="../src/frontend/menu/settings/GamepadEditor.qml" line="599"/>
         <source>guide</source>
-        <translation>гайд</translation>
+        <translation>гид</translation>
     </message>
     <message>
         <location filename="../src/frontend/menu/settings/GamepadEditor.qml" line="618"/>
@@ -152,7 +164,7 @@ in the following directories:</source>
     <message>
         <location filename="../src/frontend/menu/settings/GamepadEditor.qml" line="714"/>
         <source>hold down to quit</source>
-        <translation>Нажмите и удерживайте, чтобы закрыть</translation>
+        <translation>Удерживайте, чтобы выйти</translation>
     </message>
 </context>
 <context>
@@ -160,7 +172,7 @@ in the following directories:</source>
     <message>
         <location filename="../src/frontend/dialogs/GenericOkCancelDialog.qml" line="160"/>
         <source>Ok</source>
-        <translation>OK</translation>
+        <translation>Хорошо</translation>
     </message>
     <message>
         <location filename="../src/frontend/dialogs/GenericOkCancelDialog.qml" line="196"/>
@@ -173,7 +185,7 @@ in the following directories:</source>
     <message>
         <location filename="../src/frontend/dialogs/GenericOkDialog.qml" line="136"/>
         <source>Ok</source>
-        <translation type="unfinished">OK</translation>
+        <translation>Хорошо</translation>
     </message>
 </context>
 <context>
@@ -181,7 +193,7 @@ in the following directories:</source>
     <message>
         <location filename="../src/frontend/menu/HelpScreen.qml" line="66"/>
         <source>About</source>
-        <translation>о программе</translation>
+        <translation>О программе</translation>
     </message>
     <message>
         <location filename="../src/frontend/menu/HelpScreen.qml" line="69"/>
@@ -209,7 +221,7 @@ in the following directories:</source>
     <message>
         <location filename="../src/frontend/menu/settings/KeyEditor.qml" line="55"/>
         <source>Settings &gt; Controls</source>
-        <translation>Настройки &gt; управления</translation>
+        <translation>Настройки &gt; Управление</translation>
     </message>
 </context>
 <context>
@@ -222,7 +234,7 @@ in the following directories:</source>
     <message>
         <location filename="../src/frontend/menu/settings/keyeditor/KeyEditorEntry.qml" line="37"/>
         <source>Cancel/Back</source>
-        <translation>Отмена/Назад</translation>
+        <translation>Отменить/Назад</translation>
     </message>
     <message>
         <location filename="../src/frontend/menu/settings/keyeditor/KeyEditorEntry.qml" line="40"/>
@@ -267,7 +279,7 @@ in the following directories:</source>
         <source>Press any key now to record
 (timeout in %1 seconds)</source>
         <translation>Нажмите любую клавишу для записи
-(тайм-аут в %1 секунда)</translation>
+(время выйдет через %1 секунд)</translation>
     </message>
 </context>
 <context>
@@ -275,7 +287,7 @@ in the following directories:</source>
     <message>
         <location filename="../src/frontend/menu/settings/keyeditor/KeyEditorResetButton.qml" line="63"/>
         <source>Reset</source>
-        <translation>Сброс</translation>
+        <translation>Сбросить</translation>
     </message>
 </context>
 <context>
@@ -316,7 +328,7 @@ in the following directories:</source>
     <message>
         <location filename="../src/frontend/dialogs/MultifileSelector.qml" line="79"/>
         <source>This game has multiple entries, which one would you like to launch?</source>
-        <translation type="unfinished"></translation>
+        <translation>У этой игры несколько записей, какую из них Вы хотите запустить?</translation>
     </message>
 </context>
 <context>
@@ -329,17 +341,17 @@ in the following directories:</source>
     <message>
         <location filename="../src/frontend/messages/NoGamesError.qml" line="23"/>
         <source>Pegasus couldn&apos;t find any games on your device. If you have not set up Pegasus yet, you can find the documentation here: &lt;i&gt;%1&lt;/i&gt;.&lt;br&gt;If you still see this message, make sure your config files are readable, exist in one of the expected locations and are in the expected format.</source>
-        <translation>Pegasus не смог найти игры на устройстве. Если вы еще не настроили Pegasus, вы можете посмотреть документацию здесь: &lt;i&gt;%1&lt;/i&gt;.&lt;br&gt;Если вы еще видите это сообщение, убедитесь, что файлы конфигурации доступны для чтения и в соответствующем каталоге и в формате.</translation>
+        <translation>Pegasus не смог найти игры на вашем устройстве. Если вы ещё не настроили Pegasus, вы можете посмотреть документацию здесь: &lt;i&gt;%1&lt;/i&gt;.&lt;br&gt;Если вы всё ещё видите это сообщение, убедитесь, что файлы конфигурации доступны для чтения, находятся в соответствующем каталоге и в нужном формате.</translation>
     </message>
     <message>
         <location filename="../src/frontend/messages/NoGamesError.qml" line="30"/>
         <source>Please see the log file for more details.</source>
-        <translation>Для получения дополнительной информации см. Log-Файл.</translation>
+        <translation>Для получения дополнительной информации обратитесь к log-файлу.</translation>
     </message>
     <message>
         <location filename="../src/frontend/messages/NoGamesError.qml" line="31"/>
         <source>You can find it here:&lt;pre&gt;%1&lt;/pre&gt;</source>
-        <translation>Вы можете найти здесь:&lt;pre&gt;%1&lt;/pre&gt;</translation>
+        <translation>Вы можете найти его здесь:&lt;pre&gt;%1&lt;/pre&gt;</translation>
     </message>
 </context>
 <context>
@@ -352,7 +364,7 @@ in the following directories:</source>
     <message>
         <location filename="../src/frontend/menu/settings/ProviderEditor.qml" line="107"/>
         <source>(nothing on this platform)</source>
-        <translation>(ничего на этой платформе)</translation>
+        <translation>(ничего нет на этой платформе)</translation>
     </message>
 </context>
 <context>
@@ -373,12 +385,12 @@ in the following directories:</source>
     <message>
         <location filename="../src/frontend/menu/settings/common/ReloadQuestion.qml" line="21"/>
         <source>Reload</source>
-        <translation type="unfinished"></translation>
+        <translation>Обновить</translation>
     </message>
     <message>
         <location filename="../src/frontend/menu/settings/common/ReloadQuestion.qml" line="22"/>
         <source>Would you like to reload the game list now? This may take some time.</source>
-        <translation type="unfinished"></translation>
+        <translation>Изволите обновить список игр сейчас? Это может занять некоторое количество времени.</translation>
     </message>
 </context>
 <context>
@@ -386,12 +398,12 @@ in the following directories:</source>
     <message>
         <location filename="../src/frontend/menu/settings/SettingsMain.qml" line="60"/>
         <source>Settings</source>
-        <translation>Настройки </translation>
+        <translation>Настройки</translation>
     </message>
     <message>
         <location filename="../src/frontend/menu/settings/SettingsMain.qml" line="107"/>
         <source>General</source>
-        <translation>Главное</translation>
+        <translation>Основные</translation>
     </message>
     <message>
         <location filename="../src/frontend/menu/settings/SettingsMain.qml" line="116"/>
@@ -411,7 +423,7 @@ in the following directories:</source>
     <message>
         <location filename="../src/frontend/menu/settings/SettingsMain.qml" line="147"/>
         <source>On some platforms this setting may have no effect.</source>
-        <translation type="unfinished"></translation>
+        <translation>На некоторых платформах, данная настройка, может не возыметь эффекта.</translation>
     </message>
     <message>
         <location filename="../src/frontend/menu/settings/SettingsMain.qml" line="160"/>
@@ -421,32 +433,32 @@ in the following directories:</source>
     <message>
         <location filename="../src/frontend/menu/settings/SettingsMain.qml" line="166"/>
         <source>Change controls...</source>
-        <translation>Настройки Управления...</translation>
+        <translation>Изменить управление...</translation>
     </message>
     <message>
         <location filename="../src/frontend/menu/settings/SettingsMain.qml" line="179"/>
         <source>Change gamepad layout...</source>
-        <translation>Измените расположение Геймпада...</translation>
+        <translation>Измените раскладку геймпада...</translation>
     </message>
     <message>
         <location filename="../src/frontend/menu/settings/SettingsMain.qml" line="192"/>
         <source>Enable mouse support</source>
-        <translation type="unfinished"></translation>
+        <translation>Включить поддержку мыши</translation>
     </message>
     <message>
         <location filename="../src/frontend/menu/settings/SettingsMain.qml" line="193"/>
         <source>By default the cursor is visible if there are any pointer devices connected.</source>
-        <translation type="unfinished"></translation>
+        <translation>Курсор виден по умолчанию, если к устройству подключено любое координатное устройство.</translation>
     </message>
     <message>
         <location filename="../src/frontend/menu/settings/SettingsMain.qml" line="206"/>
         <source>Gaming</source>
-        <translation>Гейминг</translation>
+        <translation>Игры</translation>
     </message>
     <message>
         <location filename="../src/frontend/menu/settings/SettingsMain.qml" line="211"/>
         <source>Set game directories...</source>
-        <translation>Установить игровые каталоги ...</translation>
+        <translation>Выбрать игровые каталоги ...</translation>
     </message>
     <message>
         <location filename="../src/frontend/menu/settings/SettingsMain.qml" line="223"/>
@@ -459,12 +471,12 @@ in the following directories:</source>
     <message>
         <location filename="../src/frontend/dialogs/ShutdownDialog.qml" line="25"/>
         <source>Shutdown</source>
-        <translation>Отключить</translation>
+        <translation>Выключение</translation>
     </message>
     <message>
         <location filename="../src/frontend/dialogs/ShutdownDialog.qml" line="26"/>
         <source>The system will shut down. Are you sure?</source>
-        <translation>Система выключается, вы уверены?</translation>
+        <translation>Система выключится, вы уверены?</translation>
     </message>
 </context>
 <context>
@@ -485,7 +497,7 @@ in the following directories:</source>
     <message>
         <location filename="../src/frontend/messages/ThemeError.qml" line="23"/>
         <source>Pegasus tried to load the selected theme (%1), but failed. This may happen when you try to load an outdated theme, or when there&apos;s a bug in its code.</source>
-        <translation>Pegasus попытался загрузить выбранную тему (%1), но не удалось. Это может произойти, если вы попытались загрузить устаревшую тему или если код сломан.</translation>
+        <translation>Pegasus попытался загрузить выбранную тему (%1), но не смог. Это может произойти, если вы попытались загрузить устаревшую тему или если есть ошибка в её коде.</translation>
     </message>
     <message>
         <location filename="../src/frontend/messages/ThemeError.qml" line="28"/>
@@ -495,7 +507,7 @@ in the following directories:</source>
     <message>
         <location filename="../src/frontend/messages/ThemeError.qml" line="29"/>
         <source>You might find more details in the log file:&lt;pre&gt;%1&lt;/pre&gt;</source>
-        <translation>вы можете найти более подробную информацию в Log-файлах:&lt;pre&gt;%1&lt;/pre&gt;</translation>
+        <translation>Вы можете найти более подробную информацию в Log-файле:&lt;pre&gt;%1&lt;/pre&gt;</translation>
     </message>
 </context>
 <context>
@@ -503,7 +515,7 @@ in the following directories:</source>
     <message>
         <location filename="../src/frontend/main.qml" line="199"/>
         <source>Error</source>
-        <translation type="unfinished"></translation>
+        <translation>Ошибка</translation>
     </message>
 </context>
 </TS>
